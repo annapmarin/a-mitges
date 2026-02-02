9e1e18add981fbfbe358a56ddbc4090383831afd
annapmarin <annapratsmarin@gmail.com>
2026-02-02 21:21:24 +0100

    Navbar logic & styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,20 @@
 import { useState } from "react"
 import { signOut } from "firebase/auth"
 import { auth } from "../config/firebase"
+import { useNavigate } from "react-router-dom";
+import '../styles/navbar.css';
 
 export function Navbar({ user }: { user: any }) {
   const [showNav, setShowNav] = useState(false);
+  const navigate = useNavigate();
+
+  const firstName = 
+    user?.displayName?.split?.(" ")[0] ?? "Usuari";
 
   const handleLogout = async () => {
     try {
       await signOut(auth);
-      console.log("Usuari desconnectat");
+      navigate('/');
     } catch (error) {
       console.error("Error durant la desconnexió:", error);
     }
@@ -17,19 +23,23 @@ export function Navbar({ user }: { user: any }) {
 
   return (
     <nav className="navbar">
+      <div className="logo-container">
+        <img src="/logo.svg" alt="A mitges Logo" className="logo" />
+        <span>A mitges</span>
+      </div>
       <div className="user-menu">
         <button 
           className="user-button"
           onClick={() => setShowNav(!showNav)}
         >
           <img src={user.photoURL} alt={user.displayName} />
-          <span>{user.displayName}</span>
+          <span>{firstName}</span>
         </button>
         
         {showNav && (
           <div className="dropdown-menu">
             <button onClick={handleLogout}>
-              Cerrar sesión
+              Tancar sessió
             </button>
           </div>
         )}
