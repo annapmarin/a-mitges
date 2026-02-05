f38ba4ca7acd71448b7c68a38ca52fd07b0581dd
annapmarin <annapratsmarin@gmail.com>
2026-02-05 19:29:08 +0100

    fix router

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import ProjectsPage from '../pages/ProjectsPage'
 import HomePage from '../pages/HomePage'
 import CreateGroupPage from '../pages/CreateGroupPage'
@@ -7,14 +7,12 @@ import AddExpensePage from '../pages/AddExpensePage'
 
 export default function AppRoutes() {
   return (
-    <BrowserRouter basename="/a-mitges">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/projectes" element={<ProjectsPage />} />
-        <Route path="/crear-grup" element={<CreateGroupPage />} />
-        <Route path="/group/:groupId" element={<GroupPage />} />
-        <Route path="/group/:groupId/afegir-despesa" element={<AddExpensePage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/projectes" element={<ProjectsPage />} />
+      <Route path="/crear-grup" element={<CreateGroupPage />} />
+      <Route path="/group/:groupId" element={<GroupPage />} />
+      <Route path="/group/:groupId/afegir-despesa" element={<AddExpensePage />} />
+    </Routes>
   )
 }
